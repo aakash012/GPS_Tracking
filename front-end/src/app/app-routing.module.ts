80b3505c52b17063854de6306707f9ebbb3d2f50
Aakash <aakash012@live.com>
2020-09-13 18:24:29 +0530

    user-dashboard added

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 const routes: Routes = [
   {
     path: 'login',
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent
+  },
+  {
+    path: 'userDashboard',
+    component: UserDashboardComponent
   }
 ];
 
