99f1aa69c054175eaf49b448aa7ef2e382f390ea
Aakash <aakash012@live.com>
2020-09-14 19:34:37 +0530

    userLocation added

--- front-end/src/app/user-dashboard/user-dashboard.component.ts
+++ front-end/src/app/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {AgmMap, MapsAPILoader  } from '@agm/core';
+
 import {} from 'googlemaps';
 
 @Component({
@@ -6,8 +8,69 @@ import {} from 'googlemaps';
   templateUrl: './user-dashboard.component.html',
   styleUrls: ['./user-dashboard.component.css'],
 })
-export class UserDashboardComponent {
-  latitude = 28.56;
-  longitude = 77.43;
-  locationChosen = true;
+export class UserDashboardComponent implements OnInit {
+  @ViewChild(AgmMap,{static: true}) public agmMap: AgmMap;
+  lat: number;
+  lng: number;
+  getAddress: any;
+  zoom: number;
+  latitude: any;
+  longitude: any;
+  constructor(private apiloader: MapsAPILoader) {}
+  get() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position: Position) => {
+            if (position) {
+                this.lat = position.coords.latitude;
+                this.lng = position.coords.longitude;
+                this.getAddress = (this.lat, this.lng)
+                console.log(position)
+                this.apiloader.load().then(() => {
+                    let geocoder = new google.maps.Geocoder;
+                    let latlng = {
+                        lat: this.lat,
+                        lng: this.lng
+                    };
+                    geocoder.geocode({
+                        'location': latlng
+                    }, function(results) {
+                        if (results[0]) {
+                            this.currentLocation = results[0].formatted_address;
+                            console.log(this.assgin);
+                        } else {
+                            console.log('Not found');
+                        }
+                    });
+                });
+            }
+        })
+    }
+}
+ngOnInit()
+  {
+    this.get()
+    this.agmMap.triggerResize(true);
+     this.zoom = 16;
+    }
+    mapClicked($event: any) {
+      this.latitude = $event.coords.lat,
+          this.longitude = $event.coords.lng
+      this.apiloader.load().then(() => {
+          let geocoder = new google.maps.Geocoder;
+          let latlng = {
+              lat: this.latitude,
+              lng: this.longitude
+          };
+          geocoder.geocode({
+              'location': latlng
+          }, function(results) {
+              if (results[0]) {
+                  this.currentLocation = results[0].formatted_address;
+                  console.log(this.currentLocation);
+              } else {
+                  console.log('Not found');
+              }
+          });
+      });
+  }
 }
